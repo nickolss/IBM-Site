1cb87499405f88f04ca87e0b06749259ef1ea492
nickolss <nickolasmaiaraujo@gmail.com>
2023-07-05 17:15:09 -0300

    Adicionada criptografia a senha e cpf antes de inserir ao banco

--- scripts/adicionarDados.php
+++ scripts/adicionarDados.php
@@ -3,17 +3,18 @@ require_once('conexao.php');
 
 $nomeCompleto = $_POST['nome'];
 $email = $_POST['email'];
-$senha = $_POST['senha'];
+$senha = password_hash($_POST['senha'] , PASSWORD_DEFAULT);
 $telefone = $_POST['telefone'];
 $dataNasc = $_POST['dataNasc'];
-$cpf = $_POST['cpf'];
+$cpf = password_hash($_POST['cpf'] , PASSWORD_DEFAULT);
 $plano = $_POST['plano'];
 
 date_default_timezone_set("America/Sao_Paulo");
 $dataCriacaoConta = date('Y-m-d');
 
-$cpf = str_replace([".", "-"], "", $cpf); //Retirado os pontos e - do cpf
+
 $telefone = str_replace(["(", ")", "-"], "", $telefone); //Retirado os parênteses e - do telefone
+
 
 
 $sqlInsert = "INSERT INTO `Cliente`(`cpf`, `nome`, `dataNascimento`, `telefone`, `email`, `senha`, `plano`, `dataCriacaoConta`) VALUES ('$cpf','$nomeCompleto','$dataNasc','$telefone','$email','$senha','$plano','$dataCriacaoConta')";
