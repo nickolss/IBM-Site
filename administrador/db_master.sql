136265987c180468a39db16621148b19ea1b7181
nickolss <nickolasmaiaraujo@gmail.com>
2023-08-06 17:15:15 -0300

    Modificado BD do sistema

--- administrador/db_master.sql
+++ administrador/db_master.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Tempo de geração: 05-Ago-2023 às 05:22
--- Versão do servidor: 10.4.28-MariaDB
--- versão do PHP: 8.2.4
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 06-Ago-2023 às 20:14
+-- Versão do servidor: 8.0.31
+-- versão do PHP: 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -17,9 +17,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-CREATE DATABASE `db_master`;
-USE `db_master`;
-
 --
 -- Banco de dados: `db_master`
 --
@@ -27,114 +24,19 @@ USE `db_master`;
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `avaliacao`
---
-
-CREATE TABLE `avaliacao` (
-  `id` int(11) NOT NULL,
-  `escritor` varchar(200) NOT NULL,
-  `estrelas` int(11) NOT NULL,
-  `produtoAvaliado` varchar(120) NOT NULL,
-  `texto` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `cliente`
---
-
-CREATE TABLE `cliente` (
-  `id` int(11) NOT NULL,
-  `cpf` int(13) NOT NULL,
-  `nomeCompleto` varchar(200) NOT NULL,
-  `dataNasc` date NOT NULL,
-  `telefone` int(12) NOT NULL,
-  `email` varchar(150) NOT NULL,
-  `senha` varchar(20) NOT NULL,
-  `plano` enum('comum','turbinado') NOT NULL,
-  `quantidadePontos` int(11) DEFAULT NULL,
-  `fotoPerfil` blob DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `funcionario`
---
-
-CREATE TABLE `funcionario` (
-  `id` int(11) NOT NULL,
-  `rf` int(10) NOT NULL,
-  `nome` varchar(200) NOT NULL,
-  `email` varchar(200) NOT NULL,
-  `senha` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Extraindo dados da tabela `funcionario`
---
-
-INSERT INTO `funcionario` (`id`, `rf`, `nome`, `email`, `senha`) VALUES
-(1, 1, 'Nickolas Maia de Araujo', 'nickolasmaraujo@gmail.com', 'Senha123');
-
--- --------------------------------------------------------
-
---
 -- Estrutura da tabela `produto`
 --
 
-CREATE TABLE `produto` (
-  `codigoProduto` int(11) NOT NULL,
-  `nome` varchar(120) NOT NULL,
+DROP TABLE IF EXISTS `produto`;
+CREATE TABLE IF NOT EXISTS `produto` (
+  `codigoProduto` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(120) COLLATE utf8mb4_general_ci NOT NULL,
   `preco` float NOT NULL,
-  `marca` varchar(120) NOT NULL,
-  `descricao` text NOT NULL
+  `marca` varchar(120) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `descricao` text COLLATE utf8mb4_general_ci NOT NULL,
+  `customizações` enum('rebaixamento-dropped','rebaixamento-hellaFlush','pintura-solida','pintura-metalica','pintura-perolizada','pneu-solido','pneu-personalizado','pneu-duasCores','adesivo-pequeno','adesivo-medio','adesivo-grande','aerofolio','insulfilm','caixaDeSom','banco','tunagem-reformulada','tunagem-remanufaturada') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`codigoProduto`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices para tabela `cliente`
---
-ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices para tabela `funcionario`
---
-ALTER TABLE `funcionario`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices para tabela `produto`
---
-ALTER TABLE `produto`
-  ADD PRIMARY KEY (`codigoProduto`);
-
---
--- AUTO_INCREMENT de tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `cliente`
---
-ALTER TABLE `cliente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `funcionario`
---
-ALTER TABLE `funcionario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de tabela `produto`
---
-ALTER TABLE `produto`
-  MODIFY `codigoProduto` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
