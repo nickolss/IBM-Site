77671b1a5725f0082ae29cd7db9cb1d74650a444
nickolss <nickolasmaiaraujo@gmail.com>
2023-08-08 10:30:40 -0300

    inicializada lógica do login

--- assets/scripts/verificarRegistro.php
+++ assets/scripts/verificarRegistro.php
@@ -3,10 +3,35 @@ require_once('./conexao.php');
 $emailForm = $_POST['email'];
 $senhaForm = $_POST['senha'];
 
-$consultaFunc = $pdo->query("SELECT * FROM `funcionario`");
-$consultaClie = $pdo->query("SELECT * FROM `cliente`");
+$sqlCliente = $pdo->query("SELECT cliente.senha as clienteSenha FROM `cliente` WHERE cliente.email='$emailForm'");
+$quantidadeRegistrosCliente = $sqlCliente->rowCount();
+$registro = $sqlCliente->fetchAll();
+$senhaCliente = $registro[0]['clienteSenha'];
 
-$registroFunc = $consultaFunc->fetchAll();
-$registroClie = $consultaClie->fetchAll();
+if($quantidadeRegistrosCliente >=1){
+    $dadosCliente = $pdo->query("SELECT * FROM `cliente` WHERE cliente.senha='$senhaCliente'");
+    var_dump($dadosCliente->fetchAll());
+}
 
-var_dump($registroFunc);
+
+
+/*
+$sql = $pdo->query("SELECT cliente.senha as clienteSenha , funcionario.senha as funcionarioSenha FROM `cliente` , `funcionario` WHERE cliente.senha='$emailForm' || funcionario.senha='$emailForm'");
+
+$registro = $sql->fetchAll();
+var_dump($registro);
+
+// foreach ($registro as $key) {
+//     $hashCliente = $key['clienteSenha'] . "\n";
+//     $hashFuncionario = $key['funcionarioSenha'] . "\n";
+//     if (password_verify($senhaForm, $hashCliente)) {
+//         $sqlCliente = $pdo->query("SELECT * FROM `cliente` WHERE cliente.email='$emailForm'");
+//         $dadosCliente = $sqlCliente->fetchAll();
+//         var_dump($dadosCliente);
+//     } else if (password_verify($senhaForm, $hashFuncionario)) {
+//         $sqlFuncionario = $pdo->query("SELECT * FROM `funcionario` WHERE funcionario.email='$emailForm'");
+//         $dadosFuncionario = $sqlFuncionario->fetchAll();
+//         var_dump($dadosFuncionario);
+//     }
+// }
+*/
