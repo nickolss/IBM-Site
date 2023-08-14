8ff3cf24e55a220ab5d764ec391460d8d481b76d
PedroFAraujo <pedrofeearaujo@gmail.com>
2023-08-14 06:12:06 -0300

    att script cadastrarAgendamento.php

--- assets/scripts/cadastrarAgendamento.php
+++ assets/scripts/cadastrarAgendamento.php
@@ -1,6 +1,6 @@
 <?php
-    //verifica se o arquivo 'conexao.php' está incluido, se sim não irá incluir novamente
-    require_once('./conexao.php');
+    require_once('./conexao.php'); //verifica se o arquivo 'conexao.php' está incluido, se sim não irá incluir novamente
+    require_once('./iniciarSessao.php'); //verifica se o arquivo 'iniciarSessao.php' está incluindo, executando-o
 
     //passa as informações do forms para variaveis
     $cor = $_POST['corAtual'];
@@ -9,16 +9,18 @@
     $data = $_POST['data'];
     $horario = $_POST['horario'];
 
+    $id = $_SESSION['id']; 
+
     //comando sql para inserção de dados do veículo no banco
     //AINDA FALTA O CAMPO ID_CLIENTE
-    $sqlInsertCarro = "INSERT INTO `carro` (`placa`, `modelo`, `cor`) VALUES (`$placa`, `$modelo`, `$cor`)";
+    $sqlInsertCarro = "INSERT INTO `carro` (`placa`, `id_dono`,`modelo`, `cor`) VALUES (`$placa`,`$id`,`$modelo`, `$cor`)";
 
     //execução do comando sql
     $inserirDadosCarro = $pdo->prepare($sqlInsertCarro);
 
     //comando sql para inserção de dados do agendamento no banco
     //AINDA FALTA O CAMPO ID_CLIENTE
-    $sqlInsertAgendamento = "INSERT INTO `agendamento` (`data_agendamento`, `placa_carro`) VALUES (`$data`, `$placa`)";
+    $sqlInsertAgendamento = "INSERT INTO `agendamento` (`data_agendamento`,`id_cliente`,`placa_carro`) VALUES (`$data`,`$id`,`$placa`)";
 
     //execução do comando sql
     $inserirDadosAgendamento = $pdo->prepare($sqlInsertAgendamento);
