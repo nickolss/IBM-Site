6c138bd000fc4b80dd78f42dc3f6629050f5c84b
PedroFAraujo <pedrofeearaujo@gmail.com>
2023-08-19 09:11:54 -0300

    atualiazao area cadastro, add endereco

---
+++ pags/cadastro-endereco.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Turn Motors | Cadastro</title>
+
+  <!--LINK ICONES-->
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+  <link rel="stylesheet" href="../assets/css/reset.min.css">
+  <link rel="stylesheet" href="../assets/css/cadastro.min.css">
+  <link rel="stylesheet" href="../assets/css/estilos-importantes.css">
+
+  <link rel="shortcut icon" href="../assets/img/favicon.ico" type="image/x-icon">
+  
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="../assets/js/mascaraCpf.js" defer></script>
+  <script src="../assets/js/mascaraTelefone.js" defer></script>
+  <script src="../assets/js/validacaoForm.js" defer></script>
+  <script src="../assets/js/js-bootstrap/bootstrap.bundle.min.js"></script>
+  <!-- O atributo DEFER espera a página carregar para executar o Script -->
+
+</head>
+
+<body id="container__body">
+  <?php
+  require_once('../assets/components/header.php');
+  ?>
+
+  <main class="principal">
+    <form action="../assets/scripts/cadastrarCliente.php" method="POST">
+      <div class="titulo">
+        <h1 class="mainTitle">Cadastrar-se</h1>
+        <h2 class="subTitle">Veja seus pedidos de forma fácil, compre mais rápido e
+          tenha uma experiência personalizada</h2>
+      </div>
+      <div id="passos">
+        <div class="linha"></div>
+        <div class="passo">
+            <i class='bx bx-user desativado'></i>
+            <p class="legenda__icone desativado">Dados Pessoais</p>
+        </div>
+        <div class="passo">
+            <i class='bx bx-map ativado'></i>
+            <p class="legenda__icone ativado">Endereço</p>
+        </div>
+    </div>
+
+      <!--INPUTS CADASTRO ENDEREÇO-->
+
+      <section class="planos">
+        <div class="plano__background-azul">
+          <div class="plano-comum">
+            <div class="plano__cabecalho">
+              <img src="../assets/img/velocimetroDevagar.svg">
+              <h2 class="plano-comum__titulo">Plano Comum</h2>
+            </div>
+            <ul class="plano-comum__lista">
+              <li class="lista__itens-comum">Entrega segura e em todo país</li>
+              <li class="lista__itens-comum">Melhores preços entre as lojas</li>
+              <li class="lista__itens-comum">Fornecedores confiáveis</li>
+              <li class="lista__itens-comum">Pontos para utilizar em descontos</li>
+            </ul>
+
+            <button type="submit" value="comum" class="formulario__botao formulario__botao--comum" name="plano">Cadastrar-se</button>
+
+          </div>
+        </div>
+
+        <div class="plano__background-rosa">
+          <div class="plano-turbinado">
+            <div class="plano__cabecalho">
+              <img src="../assets/img/velocimetro.svg">
+              <h2 class="plano-turbinado__titulo">Plano Turbinado</h2>
+            </div>
+            <ul class="plano-turbinado__lista">
+              <li class="lista__itens-turbinado">Frete grátis e entregas mais rápidas</li>
+              <li class="lista__itens-turbinado">Acesso ao programa de pontos em dobro</li>
+              <li class="lista__itens-turbinado">Atendimento personalizado para esclarecer suas dúvidas</li>
+              <li class="lista__itens-turbinado">Brindes mensais para personalizar seu carro</li>
+              <li class="lista__itens-turbinado">Ofertas exclusivas</li>
+              <li class="lista__itens-turbinado">Cashback em algumas compras</li>
+              <li class="lista__itens-turbinado">Entrega segura e em todo país </li>
+              <li class="lista__itens-turbinado">Melhores preços entre as lojas</li>
+              <li class="lista__itens-turbinado">Fornecedores confiáveis</li>
+              <li class="lista__itens-turbinado">Pontos para utilizar em descontos</li>
+            </ul>
+            <p class="plano-turbinado__preco">apenas <span class="preco">R$29,90</span></p>
+            <a href="pagamentoTurbinado.html"><button type="submit" value="turbinado" class="formulario__botao formulario__botao--turbinado" name="plano">Cadastrar-se</button></a>
+          </div>
+        </div>
+      </section>
+
+    </form>
+  </main>
+
+  <?php
+  require_once('../assets/components/footer.php');
+  ?>
+
+
+</body>
+
+</html
--- pags/cadastro.php
+++ pags/cadastro.php
@@ -6,7 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Turn Motors | Cadastro</title>
 
-
+  <!--LINK ICONES-->
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
   <link rel="stylesheet" href="../assets/css/reset.min.css">
   <link rel="stylesheet" href="../assets/css/cadastro.min.css">
@@ -35,6 +36,17 @@
         <h2 class="subTitle">Veja seus pedidos de forma fácil, compre mais rápido e
           tenha uma experiência personalizada</h2>
       </div>
+      <div id="passos">
+        <div class="linha"></div>
+        <div class="passo">
+            <i class='bx bx-user ativado'></i>
+            <p class="legenda__icone ativado">Dados Pessoais</p>
+        </div>
+        <div class="passo">
+            <i class='bx bx-map desativado'></i>
+            <p class="legenda__icone desativado">Endereço</p>
+        </div>
+    </div>
       <div class="cadastro">
         <div class="caixa__input">
           <input type="text" required name="nome" id="nome" autocomplete="off">
@@ -71,50 +83,9 @@
         </div>
       </div>
 
-
-
-      <section class="planos">
-        <div class="plano__background-azul">
-          <div class="plano-comum">
-            <div class="plano__cabecalho">
-              <img src="../assets/img/velocimetroDevagar.svg">
-              <h2 class="plano-comum__titulo">Plano Comum</h2>
-            </div>
-            <ul class="plano-comum__lista">
-              <li class="lista__itens-comum">Entrega segura e em todo país</li>
-              <li class="lista__itens-comum">Melhores preços entre as lojas</li>
-              <li class="lista__itens-comum">Fornecedores confiáveis</li>
-              <li class="lista__itens-comum">Pontos para utilizar em descontos</li>
-            </ul>
-
-            <button type="submit" value="comum" class="formulario__botao formulario__botao--comum" name="plano">Cadastrar-se</button>
-
-          </div>
-        </div>
-
-        <div class="plano__background-rosa">
-          <div class="plano-turbinado">
-            <div class="plano__cabecalho">
-              <img src="../assets/img/velocimetro.svg">
-              <h2 class="plano-turbinado__titulo">Plano Turbinado</h2>
-            </div>
-            <ul class="plano-turbinado__lista">
-              <li class="lista__itens-turbinado">Frete grátis e entregas mais rápidas</li>
-              <li class="lista__itens-turbinado">Acesso ao programa de pontos em dobro</li>
-              <li class="lista__itens-turbinado">Atendimento personalizado para esclarecer suas dúvidas</li>
-              <li class="lista__itens-turbinado">Brindes mensais para personalizar seu carro</li>
-              <li class="lista__itens-turbinado">Ofertas exclusivas</li>
-              <li class="lista__itens-turbinado">Cashback em algumas compras</li>
-              <li class="lista__itens-turbinado">Entrega segura e em todo país </li>
-              <li class="lista__itens-turbinado">Melhores preços entre as lojas</li>
-              <li class="lista__itens-turbinado">Fornecedores confiáveis</li>
-              <li class="lista__itens-turbinado">Pontos para utilizar em descontos</li>
-            </ul>
-            <p class="plano-turbinado__preco">apenas <span class="preco">R$29,90</span></p>
-            <a href="pagamentoTurbinado.html"><button type="submit" value="turbinado" class="formulario__botao formulario__botao--turbinado" name="plano">Cadastrar-se</button></a>
-          </div>
-        </div>
-      </section>
+      <div id="div__botao__continuar">
+        <button id="btn__cadastro__continuar" type="submit">Continuar</button>
+      </div>
 
     </form>
   </main>
