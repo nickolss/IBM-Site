3fecbcd3b730c5dcddfb69f49eadee0bb03e6642
PedroFAraujo <pedrofeearaujo@gmail.com>
2023-09-11 07:19:44 -0300

    nome do banco de dados atualizado

--- assets/scripts/conexao.php
+++ assets/scripts/conexao.php
@@ -2,7 +2,7 @@
     define("MYSQL_HOST" , 'localhost:3306');
     define("MYSQL_USER" , 'root');
     define("MYSQL_PASSWORD" , '');
-    define('MYSQL_DB_NAME', 'bd_master');
+    define('MYSQL_DB_NAME', 'db_master');
 
     try {
         $pdo = new PDO('mysql:host=' . MYSQL_HOST . ';dbname=' . MYSQL_DB_NAME , MYSQL_USER , MYSQL_PASSWORD);
--- assets/scripts/conexao2.php
+++ assets/scripts/conexao2.php
@@ -2,7 +2,7 @@
     define('MYSQL_HOST', 'localhost:3306');
     define('MYSQL_USER', 'root');
     define('MYSQL_PASSWORD', '');
-    define('MYSQL_DB_NAME', 'bd_master');
+    define('MYSQL_DB_NAME', 'db_master');
     
 
     $conexao = mysqli_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB_NAME);
