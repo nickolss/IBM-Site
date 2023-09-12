a0013a2ea996891cf445fff475ef97717404ae3f
PedroFAraujo <pedrofeearaujo@gmail.com>
2023-09-12 05:52:37 -0300

    conexoes pdo e mysql

---
+++ assets/scripts/conexao2.php
@@ -0,0 +1,13 @@
+<?php
+    define('MYSQL_HOST', 'localhost:3306');
+    define('MYSQL_USER', 'root');
+    define('MYSQL_PASSWORD', '');
+    define('MYSQL_DB_NAME', 'db_master');
+    
+
+    $conexao = mysqli_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB_NAME);
+
+if (!$conexao) {
+    die("Erro na conexão com o banco de dados: " . mysqli_connect_error());
+}
+?>
