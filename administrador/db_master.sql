529f5dbd5403d994404ec325177ce7efbe04b1ab
nickolss <nickolasmaiaraujo@gmail.com>
2023-10-16 13:32:21 -0300

    Arrumado erro do nome de tabela no bd

--- administrador/db_master.sql
+++ administrador/db_master.sql
@@ -835,7 +835,7 @@ CREATE TABLE IF NOT EXISTS `produtosTroca`(
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 
 INSERT INTO
-  `produtostroca` (
+  `produtosTroca` (
     `nome`,
     `preco_pontos`,
     `descricao`,
