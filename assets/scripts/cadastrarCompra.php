bdcc81980e440f64210e8f38663e149516c003ac
PedroFAraujo <pedrofeearaujo@gmail.com>
2023-10-23 10:47:37 -0300

    script de cadastrar a compra e os pontos finalizado

--- assets/scripts/cadastrarCompra.php
+++ assets/scripts/cadastrarCompra.php
@@ -1,5 +1,64 @@
 <?php
+    //importacao dos arquivos
+    require_once('./conexao.php');
+    require_once('./iniciarSessao.php');
 
-    
+    //atribuindo o valor do id da sessao a variavel idUser
+    $idUser = $_SESSION['id'];
 
+    //para cada produto no carrinho:
+    foreach ($_SESSION['carrinho'] as $idProd => $value) {
+        $nomeProduto = $value['nome']; 
+        $quantidadeProduto = $value['quantidade'];
+        
+        //query SQL para selecionar todas as informacoes do produto
+        $sqlSelectProduto = $pdo->query("SELECT * FROM `produto` WHERE `nome`='$nomeProduto'");
+        $sqlSelectProduto->execute();
+        $produtoInfos = $sqlSelectProduto->fetchAll(PDO::FETCH_ASSOC);
+
+            //estrutura de repeticao para percorrer todos os itens e informacoes dos produtos
+            foreach($produtoInfos as $produtoInfo){
+
+                //passando as informacoes dos produtos para as variaveis correspondentes
+                $idProduto = $produtoInfo['codigoProduto'];
+                $nomeProduto = $produtoInfo['nome'];
+                $precoProduto = $produtoInfo['preco'];
+                $pontoProduto = $produtoInfo['pontos'];
+                
+                //estrutura de repeticao para fazer INSERT INTO dos produtos comprados no bd. Caso tenha mais de uma unidade de um mesmo produto o INSERT INTO sera executado de acordo com a variavel $quantidadeProduto
+                for($i=1; $i<=$quantidadeProduto; $i++){
+                    $sqlInsertCompra = $pdo->prepare("INSERT INTO `produtoscomprados` (`idProdutos`, `nomeProdutos`, `preco_final`, `id_comprador`) VALUES('$idProduto','$nomeProduto', '$precoProduto', '$idUser')");
+                }
+                
+                //caso a variavel $sqlInsertCompra tenha sido executada:
+                if($sqlInsertCompra->execute()){
+
+                    //query SQL para selecionar a quantidade de pontos da tabela cliente 
+                    $sqlSelectPontosCliente = $pdo->query("SELECT `quantidadePontos` FROM `cliente` WHERE `id`='$idUser'");
+                    $sqlSelectPontosCliente->execute();
+                    
+                    //os dados serão retornados em um array associativo 
+                    $tuplaQuantidadePontos = $sqlSelectPontosCliente->fetch(PDO::FETCH_ASSOC);
+
+                    //atribuindo o valor do registro do campo quantidadePontos, acessado pelo array, a variavel $quantidadePontosAtual
+                    $quantidadePontosAtual = $tuplaQuantidadePontos['quantidadePontos'];
+
+                    //atribuindo a variavel $quantidadePontoFinal a soma entre a quantidade de pontos atual do cliente e a quantidade de pontos do produto especifico
+                    $quantidadePontoFinal = $pontoProduto + $quantidadePontosAtual;
+
+                    //atualizando o campo 'quantidadePontos' e atribuindo o novo valor sendo a quantidade final de pontos
+                    $sqlInsertPontosCliente = $pdo->prepare("UPDATE `cliente` SET `quantidadePontos`='$quantidadePontoFinal' WHERE `id`='$idUser' LIMIT 1");
+
+                    //caso a variavel $sqlInsertPontosCliente tenha sido executada:
+                    if($sqlInsertPontosCliente->execute()){
+                        $tituloModal = "Compra realizada com sucesso!";
+                        $textoModal = "Obrigado pela preferência!";
+                        require_once('../components/modal.php');
+                    }
+                }
+            }
+
+        
+
+    }
 ?>
