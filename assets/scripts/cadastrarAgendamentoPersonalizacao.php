d463c0471ed5e1310f331f4eda5ad2eb13d5db67
PedroFAraujo <pedrofeearaujo@gmail.com>
2023-10-24 14:04:58 -0300

    modal em cada etapa do sistema de agendamento

--- assets/scripts/cadastrarAgendamentoPersonalizacao.php
+++ assets/scripts/cadastrarAgendamentoPersonalizacao.php
@@ -1,63 +1,65 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/modal.min.css">
+        <title>Turn Motors</title>
+    </head>
+    <body>
+        <?php
 
-use function PHPSTORM_META\type;
+            use function PHPSTORM_META\type;
 
-require_once('conexao.php'); //requirindo o arquivo conexao.php
-require_once('./iniciarSessao.php'); //verifica se o arquivo 'iniciarSessao.php' está incluindo, executando-o
+            require_once('conexao.php'); //requirindo o arquivo conexao.php
+            require_once('./iniciarSessao.php'); //verifica se o arquivo 'iniciarSessao.php' está incluindo, executando-o
 
-//atribuindo as variaveis os valores que vieram do formulario
-$personalizacao = $_POST['personalizacao'];
-$btnValue = $_POST['btn-pedido-orcamento'];
-$idDono = $_SESSION['id'];
+            //atribuindo as variaveis os valores que vieram do formulario
+            $personalizacao = $_POST['personalizacao'];
+            $btnValue = $_POST['btn-pedido-orcamento'];
+            $idDono = $_SESSION['id'];
 
+            $sqlVeiculo = $pdo->query("SELECT `placaCarro`, `data`, `horario` FROM `pedido_orcamento` WHERE id_cliente = '$idDono' && status = 'cliente confirmado'");
+            $pedidoOrcamento = $sqlVeiculo->fetchAll();
+            $placa = $pedidoOrcamento[0]['placaCarro'];
+            $data = $pedidoOrcamento[0]['data'];
+            $horario = $pedidoOrcamento[0]['horario'];
 
-$sqlVeiculo = $pdo->query("SELECT `placaCarro`, `data`, `horario` FROM `pedido_orcamento` WHERE id_cliente = '$idDono' && status = 'cliente confirmado'");
-$pedidoOrcamento = $sqlVeiculo->fetchAll();
-$placa = $pedidoOrcamento[0]['placaCarro'];
-$data = $pedidoOrcamento[0]['data'];
-$horario = $pedidoOrcamento[0]['horario'];
+            $id = (int)$_SESSION['id']; //atribuindo o 'id' da sessão atual para a variável $id
 
+            $sql = "INSERT INTO `agendamento` (`data_agendamento`, `horario`, `id_cliente`, `placa_carro`, `agendamento`) VALUES ('$data', '$horario', $id, '$placa', '$personalizacao')";
 
+            $stmt = $pdo->prepare($sql);
 
-$id = (int)$_SESSION['id']; //atribuindo o 'id' da sessão atual para a variável $id
+            if ($stmt->execute()) {
 
-$sql = "INSERT INTO `agendamento` (`data_agendamento`, `horario`, `id_cliente`, `placa_carro`, `agendamento`) VALUES ('$data', '$horario', $id, '$placa', '$personalizacao')";
+                if ($btnValue == "confirmado") {
 
-$stmt = $pdo->prepare($sql);
+                    $query_update_orcamento = "UPDATE `pedido_orcamento` 
+                                                SET `status`='agendamento confirmado'
+                                                WHERE `personalizacao`='$personalizacao' AND `placaCarro`='$placa' AND `id_cliente`='$id'
+                                                LIMIT 1";
+                    $stmtOrcamento = $pdo->prepare($query_update_orcamento);
 
-if ($stmt->execute()) {
+                    if ($stmtOrcamento->execute()) {
+                        $tituloModal = "Agendamento Confirmado!";
+                        $textoModal = "Obrigado por escolher a Turn Motors!";
+                        require_once("../components/modal.php");
+                    }
+                } else if ($btnValue == "cancelado") {
+                    $query_update_orcamento = "UPDATE `pedido_orcamento` 
+                                                SET `status`='cliente cancelado'
+                                                WHERE `personalizacao`='$personalizacao' AND `placaCarro`='$placa' AND `id_cliente`='$id'
+                                                LIMIT 1";
+                    $stmtOrcamento = $pdo->prepare($query_update_orcamento);
 
-    if ($btnValue == "confirmado") {
-
-        $query_update_orcamento = "UPDATE `pedido_orcamento` 
-                                    SET `status`='agendamento confirmado'
-                                    WHERE `personalizacao`='$personalizacao' AND `placaCarro`='$placa' AND `id_cliente`='$id'
-                                    LIMIT 1";
-        $stmtOrcamento = $pdo->prepare($query_update_orcamento);
-
-        if ($stmtOrcamento->execute()) {
-            $tituloModal = "Agendamento Confirmado!";
-            $textoModal = "Obrigado por escolher a Turn Motors!";
-            require_once("../components/modal.php");
-            /* QUANDO SISTEMA DE ENVIAR EMAIL ESTIVER PRONTO
-                    echo "<script>
-                        alert('Agendamento confirmado com sucesso.');
-                        setInterval( function() {
-                            window.location.href = 'enviarEmailAgendamento.php'
-                        }, 0)
-                    </script>";*/
-        }
-    } else if ($btnValue == "cancelado") {
-        $query_update_orcamento = "UPDATE `pedido_orcamento` 
-                                    SET `status`='cliente cancelado'
-                                    WHERE `personalizacao`='$personalizacao' AND `placaCarro`='$placa' AND `id_cliente`='$id'
-                                    LIMIT 1";
-        $stmtOrcamento = $pdo->prepare($query_update_orcamento);
-
-        if ($stmtOrcamento->execute()) {
-            $tituloModal = "Agendamento Cancelado!";
-            $textoModal = "Esperamos que volte a comprar conosco.";
-            require_once("../components/modal.php");
-        }
-    }
-}
+                    if ($stmtOrcamento->execute()) {
+                        $tituloModal = "Agendamento Cancelado!";
+                        $textoModal = "Esperamos que volte a comprar conosco. :'(";
+                        require_once("../components/modal.php");
+                    }
+                }
+            }
+        ?>
+    </body>
+</html>
--- assets/scripts/cadastrarOrcamentoCliente.php
+++ assets/scripts/cadastrarOrcamentoCliente.php
@@ -1,40 +1,51 @@
-<?php
-    require_once('conexao.php'); //requirindo o arquivo conexao.php
-    require_once('./iniciarSessao.php'); //verifica se o arquivo 'iniciarSessao.php' está incluindo, executando-o
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/modal.min.css">
+        <title>Turn Motors</title>
+    </head>
+    <body>
+        <?php
+            require_once('conexao.php'); //requirindo o arquivo conexao.php
+            require_once('./iniciarSessao.php'); //verifica se o arquivo 'iniciarSessao.php' está incluindo, executando-o
 
-    //atribuindo as variaveis os valores que vieram do formulario
-    $personalizacao = $_POST['personalizacao'];
-    $placa = $_POST['placaCarro'];
-    $btnValue = $_POST['btn-pedido-orcamento']; 
-    
-    $id = (int)$_SESSION['id']; //atribuindo o 'id' da sessão atual para a variável $id
+            //atribuindo as variaveis os valores que vieram do formulario
+            $personalizacao = $_POST['personalizacao'];
+            $placa = $_POST['placaCarro'];
+            $btnValue = $_POST['btn-pedido-orcamento']; 
+            
+            $id = (int)$_SESSION['id']; //atribuindo o 'id' da sessão atual para a variável $id
 
-    //se o valor do botao for "cancelado", irá fazer uma query SQL atualizando o campo "status" para "cliente cancelado" e redireciona para a página para a de perfil
-    if($btnValue == "cancelado"){
-        $query_update_orcamento = "UPDATE `pedido_orcamento` 
-                                SET `status`='cliente cancelado', `preco`=''
-                                WHERE `personalizacao`='$personalizacao' AND `placaCarro`='$placa' AND `id_cliente`='$id'
-                                LIMIT 1";
-        $stmt = $pdo->prepare($query_update_orcamento);
+            //se o valor do botao for "cancelado", irá fazer uma query SQL atualizando o campo "status" para "cliente cancelado" e redireciona para a página para a de perfil
+            if($btnValue == "cancelado"){
+                $query_update_orcamento = "UPDATE `pedido_orcamento` 
+                                        SET `status`='cliente cancelado', `preco`=''
+                                        WHERE `personalizacao`='$personalizacao' AND `placaCarro`='$placa' AND `id_cliente`='$id'
+                                        LIMIT 1";
+                $stmt = $pdo->prepare($query_update_orcamento);
 
-        if($stmt->execute()){
-            $tituloModal = "Orçamento Cancelado!";
-            $textoModal = "Esperamos que volte a comprar conosco.";
-            require_once("../components/modal.php");
-        }
+                if($stmt->execute()){
+                    $tituloModal = "Orçamento Cancelado!";
+                    $textoModal = "Esperamos que volte a comprar conosco.";
+                    require_once("../components/modal.php");
+                }
 
-    //se o valor do botao for "confirmado", irá fazer uma query SQL atualizando o campo "status" para "confirmado" e o campo "preco" com seu preco correspondente, logo após redireciona para a página de Cadastrar Orçamento
-    }else if($btnValue == "confirmado"){
-        $query_update_orcamentoConfirmado = "UPDATE `pedido_orcamento` 
-                                SET `status`='cliente confirmado'
-                                WHERE `personalizacao`='$personalizacao' AND `placaCarro`='$placa' AND `id_cliente`='$id'
-                                LIMIT 1";
-        $stmtConfirmado = $pdo->query($query_update_orcamentoConfirmado);
+            //se o valor do botao for "confirmado", irá fazer uma query SQL atualizando o campo "status" para "confirmado" e o campo "preco" com seu preco correspondente, logo após redireciona para a página de Cadastrar Orçamento
+            }else if($btnValue == "confirmado"){
+                $query_update_orcamentoConfirmado = "UPDATE `pedido_orcamento` 
+                                        SET `status`='cliente confirmado'
+                                        WHERE `personalizacao`='$personalizacao' AND `placaCarro`='$placa' AND `id_cliente`='$id'
+                                        LIMIT 1";
+                $stmtConfirmado = $pdo->query($query_update_orcamentoConfirmado);
 
-        if($stmtConfirmado->execute()){
-            $tituloModal = "Orçamento Confirmado com Sucesso!";
-            $textoModal = "Agradecemos por comprar com a Turn Motors.";
-            require_once("../components/modal.php");
-        }
-    }
-?>
+                if($stmtConfirmado->execute()){
+                    $tituloModal = "Orçamento Confirmado com Sucesso!";
+                    $textoModal = "Agradecemos por comprar com a Turn Motors.";
+                    require_once("../components/modal.php");
+                }
+            }
+        ?>
+    </body>
+</html>
