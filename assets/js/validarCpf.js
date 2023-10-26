e87f7274cd88650298901b58652a4fdf91240e0c
nickolss <nickolasmaiaraujo@gmail.com>
2023-10-26 13:37:10 -0300

    Finalizado validação de cpf no cadastro

--- assets/js/validarCpf.js
+++ assets/js/validarCpf.js
@@ -1,9 +1,10 @@
 const inputCpf = document.querySelector("#cpf");
+const divCpf = document.querySelector("#inputCpf");
 
 const erroLog = document.createElement("p");
 erroLog.textContent = "CPF Inválido";
-inputCpf.appendChild(erroLog);
 erroLog.classList.add("hidden");
+divCpf.appendChild(erroLog);
 
 function validaCPF(cpf) {
   const url = `https://api.invertexto.com/v1/validator?token=5116%7CEzc50pZoryHRrAC702HSzsS3dD2RPhB8&value=${cpf}&type=cpf`;
@@ -17,6 +18,8 @@ function validaCPF(cpf) {
         erroLog.classList.add("hidden");
       }
     });
+
+  console.log(inputCpf.value)
 }
 
 inputCpf.addEventListener("keyup", () => {
