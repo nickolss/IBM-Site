fe6a8a6fbbe1612ad87f8c2fe114d6f6ec68769e
PedroFAraujo <pedrofeearaujo@gmail.com>
2023-11-03 06:21:26 -0300

    estilizado modal script cadastrarAvaliacao

--- assets/scripts/cadastrarAvaliacao.php
+++ assets/scripts/cadastrarAvaliacao.php
@@ -1,15 +1,27 @@
-<?php
-require_once("conexao.php");
-require_once("iniciarSessao.php");
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/modal.min.css">
+        <title>Turn Motors</title>
+    </head>
+    <body>
+        <?php
+            require_once("conexao.php");
+            require_once("iniciarSessao.php");
 
-$idEscritor = $_SESSION['id'];
-$idProduto = $_GET['id_produto'];
-$comentario = $_POST['comentario'];
+            $idEscritor = $_SESSION['id'];
+            $idProduto = $_GET['id_produto'];
+            $comentario = $_POST['comentario'];
 
-$sqlInsertAvaliacao = $pdo->prepare("INSERT INTO `avaliacao`(`id_escritor`, `id_produto`, `texto`) VALUES ('$idEscritor','$idProduto','$comentario') LIMIT 1");
+            $sqlInsertAvaliacao = $pdo->prepare("INSERT INTO `avaliacao`(`id_escritor`, `id_produto`, `texto`) VALUES ('$idEscritor','$idProduto','$comentario') LIMIT 1");
 
-if ($sqlInsertAvaliacao->execute()) {
-    $tituloModal = "Obrigado pela avaliação!";
-    $textoModal = "Continue comprando conosco e ajudando a fortalecer a comunidade Turn Motors.";
-    require_once('../components/modal.php');
-}
+            if ($sqlInsertAvaliacao->execute()) {
+                $tituloModal = "Obrigado pela avaliação!";
+                $textoModal = "Continue comprando conosco e ajudando a fortalecer a comunidade Turn Motors.";
+                require_once('../components/modal.php');
+            }
+        ?>
+    </body>
+</html>
