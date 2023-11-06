ce5b665014f71d62a8ae924de4b7bb1f525c9ec5
PedroFAraujo <pedrofeearaujo@gmail.com>
2023-11-06 14:04:01 -0300

    modal ao excluirComentario estilizado

--- assets/scripts/excluirComentario.php
+++ assets/scripts/excluirComentario.php
@@ -1,12 +1,24 @@
-<?php
-require_once("./conexao.php");
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/modal.min.css">
+        <title>Turn Motors</title>
+    </head>
+    <body>
+        <?php
+            require_once("./conexao.php");
 
-$idProd = $_GET['mercadoria'];
-$idComprador = $_GET["comprador"];
-$sqlDelete = $pdo->prepare("DELETE FROM `avaliacao` WHERE id_produto=$idProd && id_escritor=$idComprador");
+            $idProd = $_GET['mercadoria'];
+            $idComprador = $_GET["comprador"];
+            $sqlDelete = $pdo->prepare("DELETE FROM `avaliacao` WHERE id_produto=$idProd && id_escritor=$idComprador");
 
-if ($sqlDelete->execute()) {
-    $tituloModal = "Avaliação Excluída";
-    $textoModal = "Continue avaliando e ajudando a fortalecer a comunidade Turn Motors.";
-    require_once('../components/modal.php');
-}
+            if ($sqlDelete->execute()) {
+                $tituloModal = "Avaliação Excluída";
+                $textoModal = "Continue avaliando e ajudando a fortalecer a comunidade Turn Motors.";
+                require_once('../components/modal.php');
+            }
+        ?>
+    </body>
+</html>
